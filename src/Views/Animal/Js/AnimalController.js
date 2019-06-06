b8fef2bb2612424a1e65f33f98d002110f875a9e
James <jamesventosa@hotmail.com>
2019-06-06 00:44:30 -0300

    Adicionando a tela do perfil do animal #33

---
+++ src/Views/Animal/Js/AnimalController.js
@@ -0,0 +1,24 @@
+$(function() {
+    // Handler for .ready() called.
+});
+
+$(document).on("submit", "#form-cad-animal",function(e){
+    e.preventDefault();
+    var postURL = "../../Controllers/AnimalController.php?action=Adicionar";
+
+    $.ajax({
+        type: "POST",
+        url: postURL,
+        data: $("#form-cad-animal").serialize(),
+        success: function(data){
+            var result = JSON.parse(data);
+            if(result.success){
+                alert("Animal adicionado com sucesso!!! :)");
+            }else{
+                alert("Erro ao adicionar um animal!!! :(");
+            }
+        },
+        error: function(xhr, status, error) {
+        }
+    });
+ });
---
+++ src/Views/Animal/index.php
@@ -0,0 +1,17 @@
+<?php ?>
+<script type="text/javascript" src="../../Assets/Js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="./Js/AnimalController.js"></script>
+
+
+
+
+<form id="form-cad-animal" method="Post">
+<input type="text" name="nome" placeholder="nome" />
+<input type="text" name="especie" placeholder="especie" />
+<input type="text" name="genero" placeholder="genero" />
+<input type="text" name="situacao" placeholder="situacao" />
+<input type="text" name="dataNascimento" placeholder="dataNascimento" />
+<input type="text" name="historico" placeholder="historico" />
+<input type="text" name="raca" placeholder="raca" />
+<input type="submit" value="Sign in" />
+</form>
