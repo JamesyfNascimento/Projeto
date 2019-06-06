b37397cc2846bb0a603e71895f8fdc3741902ce4
James <jamesventosa@hotmail.com>
2019-06-06 00:35:02 -0300

    Alterando classe do AnimalRepositorio. Metodo Adicionar implementado #22 #32

--- src/Infra/Repositorios/AnimalRepositorio.php
+++ src/Infra/Repositorios/AnimalRepositorio.php
@@ -29,5 +29,47 @@ class AnimalRepositorio extends BaseRepositorio implements IAnimalRepositorio {
       parent::__construct();
        $this->tabela = "Animal";
     }
+
+
+    /**
+       *
+       * Adicionar: Insere informações do animal no banco de dados
+       * Retorna: true para sucesso na criação,
+       *          False para erro na criação
+       * @param object $objeto
+       * @return boolean
+       */
+      public function Adicionar( $objeto ) {
+        $nome = $objeto->getNome();
+        $especie = $objeto->getEspecie();
+        $genero = $objeto->getGenero();
+        $situacao = $objeto->getSituacao();
+        $data_nascimento = $objeto->getDataNascimento();
+        $historico = $objeto->getHistorico();
+        $raca = $objeto->getRaca();
+
+        $sqlStmt = "INSERT INTO {$this->tabela} (NOME, ESPECIE, GENERO, SITUACAO, DATANASCIMENTO, HISTORICO, RACA) VALUES (:nome, :especie, :genero, :situacao, :nascimento, :historico, :raca)";
+        try {
+           $operacao = $this->conexao->prepare($sqlStmt);
+           $operacao->bindValue(":nome", $nome, PDO::PARAM_STR);
+           $operacao->bindValue(":especie", $especie, PDO::PARAM_STR);
+           $operacao->bindValue(":genero", $genero, PDO::PARAM_STR);
+           $operacao->bindValue(":situacao", $situacao, PDO::PARAM_STR);
+           $operacao->bindValue(":nascimento", $data_nascimento, PDO::PARAM_STR);
+           $operacao->bindValue(":historico", $historico, PDO::PARAM_STR);
+           $operacao->bindValue(":raca", $raca, PDO::PARAM_STR);
+           if($operacao->execute()){
+                 if($operacao->rowCount() > 0) {
+                    return true;
+                 } else {
+                    return false;
+                 }
+              } else {
+                 return false;
+           }
+        } catch( PDOException $excecao ) {
+              echo $excecao->getMessage();
+        }
+     }
 }
 ?>
