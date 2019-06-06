d27d0a18bccb201cb4063746caff15885ca6b14d
James <jamesventosa@hotmail.com>
2019-06-06 00:30:51 -0300

    Adicionando interface do animalRepositorio #32

---
+++ src/Infra/Interfaces/IAnimalRepositorio.php
@@ -0,0 +1,14 @@
+<?php  
+namespace Infra\Interfaces;
+/**
+* Descrição da interface IAnimalDao – representa as funcionalidade que a classe AnimalDao deve suportar.
+*
+* @author James Yuri
+*/
+require_once 'IBaseRepositorio.php';
+use Infra\Interfaces\IBaseRepositorio;
+
+    interface IAnimalRepositorio extends IBaseRepositorio {
+    
+   }
+?>
